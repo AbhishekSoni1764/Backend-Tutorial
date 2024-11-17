ca58584126a38db9a17abc47e6f4c0d748c2452f
Abhishek Soni <abhisoni1764@gmail.com>
2024-11-17 16:56:33 +0530

    toggleCommentLike controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -25,7 +25,6 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
         });
 
         if (!isLiked) {
-            // If not liked, create a like
             const likeVideo = await Like.create({
                 video: videoId,
                 likedBy: user
@@ -39,7 +38,6 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
                 new ApiResponse(200, {}, "Video liked successfully!")
             );
         } else {
-            // If already liked, remove the like
             const unLikeVideo = await Like.findByIdAndDelete(isLiked._id);
             if (!unLikeVideo) {
                 throw new ApiError(401, "Video's like was not removed!");
@@ -60,7 +58,51 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const { commentId } = req.params
-    //TODO: toggle like on comment
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(401, "VideoId is not valid!");
+    }
+
+    try {
+        const user = req.user?._id;
+
+        if (!user) {
+            throw new ApiError(401, "User not found!");
+        }
+
+        const isLiked = await Like.findOne({
+            comment: commentId,
+            likedBy: user
+        });
+
+        if (!isLiked) {
+            const likeComment = await Like.create({
+                comment: commentId,
+                likedBy: user
+            });
+
+            if (!likeComment) {
+                throw new ApiError(401, "Comment was not liked!");
+            }
+
+            return res.status(200).json(
+                new ApiResponse(200, {}, "Comment liked successfully!")
+            );
+        } else {
+            const unLikeComment = await Like.findByIdAndDelete(isLiked._id);
+            if (!unLikeComment) {
+                throw new ApiError(401, "Comment's like was not removed!");
+            }
+
+            return res.status(200).json(
+                new ApiResponse(200, {}, "Comment unliked successfully!")
+            );
+        }
+    } catch (error) {
+        throw new ApiError(
+            400,
+            error?.message || "Something went wrong while toggling the like!"
+        );
+    }
 
 })
 
