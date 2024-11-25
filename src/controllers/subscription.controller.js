150968df8d68a269c93366096d475a538e11a635
Abhishek Soni <abhisoni1764@gmail.com>
2024-11-26 04:13:48 +0530

    toggleSubscription Controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -8,7 +8,52 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const { channelId } = req.params
-    // TODO: toggle subscription
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(401, "ChannelId is not valid!!")
+    }
+
+    const subscriber = await Subscription.findOne({
+        $and: [{ channel: channelId }, { subscribers: req.user?._id }]
+    })
+
+    if (!subscriber) {
+        const subscribe = await Subscription.create({
+            channel: channelId,
+            subscribers: req.user?._id,
+        })
+
+        if (!subscribe) {
+            throw new ApiError(401, "Subscription unsuccessfull!!")
+        }
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    subscribe,
+                    "Subscription Successfull!!"
+                )
+            )
+    }
+
+    const unsubscribe = await Subscription.findByIdAndDelete(subscriber?._id)
+
+    if (!unsubscribe) {
+        throw new ApiError(401, "Unsubscription Unsuccessfull!!")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Unsubscription Successfull!!"
+            )
+        )
+
 })
 
 // controller to return subscriber list of a channel
