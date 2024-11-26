538b8e17cb1c1de5e51c5e103f74425d32e96321
Abhishek Soni <abhisoni1764@gmail.com>
2024-11-26 18:42:39 +0530

    getChannelVideos controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -11,7 +11,50 @@ const getChannelStats = asyncHandler(async (req, res) => {
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
+    const userId = req.user?._id;
+
+    if (!userId) {
+        throw new ApiError(401, "UserId invalid!!")
+    }
+
+    try {
+        const videos = await Video.aggregate([
+            {
+                $match: {
+                    owner: new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $project: {
+                    videoFile: 1,
+                    title: 1,
+                    description: 1,
+                    duration: 1,
+                    views: 1,
+                    isPublished: 1,
+                    owner: 1,
+                    createdAt: 1
+
+                }
+            }
+        ])
+
+        if (!videos) {
+            throw new ApiError(401, "No Videos were found!!")
+        }
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    videos,
+                    "Channel Videos Successfully fetched!!"
+                )
+            )
+    } catch (error) {
+        throw new ApiError(400, error?.message || "Something went wrong while fetching channel videos!!")
+    }
 })
 
 export {
