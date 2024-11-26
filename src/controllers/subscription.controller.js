d808b5240424ab55689fa08f325a5fdafba8e02c
Abhishek Soni <abhisoni1764@gmail.com>
2024-11-26 20:05:28 +0530

    Final Commit

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -132,7 +132,6 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     }
 })
 
-// controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
 
